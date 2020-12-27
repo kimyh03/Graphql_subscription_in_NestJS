e5a9b0d0b147a16dab906b067c900800ef30da52
Hoony <kimyh03@gmail.com>
2020-12-27 19:27:32 +0900

    Edit prisma schema

--- src/auth/assignUser.middleware.ts
+++ src/auth/assignUser.middleware.ts
@@ -11,7 +11,6 @@ export class AssignUserMiddleware implements NestMiddleware {
   ) {}
   async use(req: any, res: Response, next: NextFunction) {
     const token = req.headers?.authorization?.split(' ')[1];
-    console.log(token);
     if (token) {
       try {
         const payload = this.authService.verify(token);
